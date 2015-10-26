bd041188331c3a87c8c6e9bbb43460ddff441b81
ramanmittal <rmittal@greenapplesolutions.com>
2015-10-26 19:29:50 +0530

    add create teacher route

--- SchoolApp/SchoolWeb/angularjs/route.js
+++ SchoolApp/SchoolWeb/angularjs/route.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module(appname);
+var app = angular.module(appname);
 app.config(['$urlRouterProvider', '$stateProvider', '$controllerProvider', function (urp, sp, $controllerProvider) {
     var route = function (url) {
         var obj = {};
@@ -31,5 +31,6 @@ app.config(['$urlRouterProvider', '$stateProvider', '$controllerProvider', funct
 
     sp.state('createclasslabel', { url: '/createclasslabel', templateUrl: '/templates/master/CreateClassLabel.html', controller: 'classController', resolve: route(['/javascripts/controllers/masters/classcontroller.js']) });
     sp.state('teachers', { url: '/teachers', templateUrl: '/templates/master/teachers.html', controller: 'teacherController', resolve: route(['/javascripts/controllers/masters/teachercontroller.js']) });
+    sp.state('createteacher', { url: '/createteacher', templateUrl: '/templates/teacher/createteacher.html', controller: 'teacherController', resolve: route(['/javascripts/controllers/masters/teachercontroller.js']) });
 
 }]);
